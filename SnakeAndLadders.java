38c06cfe46c885533c65036485a6bf2da5a25005
dinesh nangare <dineshnangare1717@gmail.com>
2022-03-28 18:08:24 +0530

    dice count

--- SnakeAndLadders.java
+++ SnakeAndLadders.java
@@ -1,17 +1,20 @@
 package SnakeAndLadder;
 import java.util.Random;
 
-//UC3
+
 public class SnakeAndLadders {
     public static void main(String[] args) {
         int position = 0;
+        int IS_NO_PLAY = 0;
         int IS_LADDER = 2;
         int IS_SNAKE = 1;
-        int NO_PLAY = 0;
         int WINNING_POSITION = 100;
+        int diceCount = 0;
+
         System.out.println("Welcome to Snake and Ladder Game");
 
         while (position < WINNING_POSITION) {
+            diceCount++;
             Random random = new Random();
             int diceNumber = random.nextInt(6) + 1;
             int option = random.nextInt(3);
@@ -31,7 +34,9 @@ public class SnakeAndLadders {
                 System.out.println("No Play for the Player");
             }
             System.out.println("Dice Number : " + diceNumber);
+            System.out.println(); // Just used to print empty line.
             System.out.println("Position : " + position);
+            System.out.println("Dice Count : " + diceCount);
         }
     }
 }
