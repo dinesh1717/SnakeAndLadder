a391af801ea2311e8c138070255b67476e6f207f
dinesh nangare <dineshnangare1717@gmail.com>
2022-03-28 17:44:44 +0530

    dice

--- SnakeAndLadders.java
+++ SnakeAndLadders.java
@@ -1,8 +1,14 @@
 package SnakeAndLadder;
-
+import java.util.Random;
+//UC2
 public class SnakeAndLadders {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game");
         int startPosition = 0;
+        Random random = new Random();
+        int diceNumber = random.nextInt(6)+1;
+        System.out.println("Dice Number : " + diceNumber);
     }
 }
+
+
