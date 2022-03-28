c07e6ba3369c36d03f545d9fe6ce4fdaa312c1fe
dinesh nangare <dineshnangare1717@gmail.com>
2022-03-28 17:38:17 +0530

    Start Point

---
+++ SnakeAndLadders.java
@@ -0,0 +1,8 @@
+package SnakeAndLadder;
+
+public class SnakeAndLadders {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Game");
+        int startPosition = 0;
+    }
+}
